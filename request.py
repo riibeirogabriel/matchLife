e77215d9c943a1b31851cfef60e1bf7af220ec7d
Gabriel Ribeiro <gabbriel.rribeiro@gmail.com>
2020-04-19 20:43:12 -0300

    requests APIs

---
+++ request.py
@@ -0,0 +1,47 @@
+import requests 
+import json
+
+
+api_serpro_cpf_url = "https://gateway.gr1d.io/sandbox/serpro/consulta-cpf/v1/cpf/"
+api_mongeral_aegon_insurance = "https://gateway.gr1d.io/sandbox/mongeralaegon/v1/simulacao"
+
+
+def cpf_analize(cpf_number):    
+    headers = { "X-Api-Key": "7daeffeb-c080-4ac3-8181-4a670d6abdb1"}
+    response = requests.get(api_serpro_cpf_url + str(cpf_number), headers = headers) 
+    json_object = json.loads(json.dumps(response.json()))
+    return json.dumps(json_object, indent=2)
+
+
+def person_insurance_simulation(cnpj, codigoModeloProposta, name, cpf, birthDate, work, earn, gender, uf ):
+    params = (("cnpj",cnpj), ("codigoModeloProposta",codigoModeloProposta))
+    headers= {"X-Api-Key": "1e0d86b2-d0b2-4ddb-96b4-cdc907f7e5a8"}
+    body =  {
+            "simulacoes": [
+                {
+                "proponente": {
+                    "tipoRelacaoSeguradoId": 1,
+                    "nome": name,
+                    "cpf":  cpf,
+                    "dataNascimento": birthDate,
+                    "profissaoCbo": work,
+                    "renda": earn,
+                    "sexoId": gender,
+                    "uf": uf,
+                    "declaracaoIRId": 1
+                },
+                "periodicidadeCobrancaId": 30,
+                "prazoCerto": 30
+                }
+            ]
+            }
+
+    response = requests.post(api_mongeral_aegon_insurance, params = params, headers = headers, json= body )
+    json_object = json.loads(json.dumps(response.json()))
+    return json.dumps(json_object, indent=2)
+
+print(cpf_analize(11855727676))
+
+print(person_insurance_simulation(11321351000110, "YZ", "Luis Inacio gonzaga",
+                                  11855727676, "1980-10-20T00:00:00", "2410-05",
+                                  5000.0, 1, "MA"))
